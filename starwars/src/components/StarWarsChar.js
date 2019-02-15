abe00d850ecd41b076dcd621478eab09cd16a3ef
Julian Moreno <julmo611@gmail.com>
2019-02-15 10:50:11 -0800

    MVP done!

--- starwars/src/components/StarWarsChar.js
+++ starwars/src/components/StarWarsChar.js
@@ -4,14 +4,14 @@ import './StarWars.scss';
 const StarWarsChar = (props) => {
     return (
         <div className="char-box">
-            <div>{props.obj.name}</div>
-            <div>{props.obj.birth_year}</div>
-            <div>{props.obj.eye_color}</div>
-            <div>{props.obj.hair_color}</div>
-            <div>{props.obj.skin_color}</div>
-            <div>{props.obj.gender}</div>
-            <div>{props.obj.height}</div>
-            <div>{props.obj.mass}</div>
+            <h2>{props.obj.name}</h2>
+           <div className="p-line"><p><strong>DOB:</strong></p><p>{props.obj.birth_year}</p></div>
+            <div className="p-line"><p><strong>Eyes Color:</strong></p><p>{props.obj.eye_color}</p></div>
+            <div className="p-line"><p><strong>Hair Color:</strong></p><p>{props.obj.hair_color}</p></div>
+            <div className="p-line"><p><strong>Skin Color:</strong></p><p>{props.obj.skin_color}</p></div>
+            <div className="p-line"><p><strong>Gender:</strong></p><p>{props.obj.gender}</p></div>
+            <div className="p-line"><p><strong>Height:</strong></p><p>{props.obj.height}</p></div>
+            <div className="p-line"><p><strong>Mass:</strong></p><p>{props.obj.mass}</p></div>
         </div>
     )
 }
--- starwars/src/components/StarWarsList.js
+++ starwars/src/components/StarWarsList.js
@@ -7,7 +7,7 @@ import StarWarsChar from './StarWarsChar';
 const StarWarsList = (props) => {
     
     return (
-        <div>
+        <div className="cards-block">
             {props.value.map(character => <StarWarsChar obj={character} key={character.created} />) }
         </div>
     );
